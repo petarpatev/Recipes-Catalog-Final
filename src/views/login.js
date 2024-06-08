5450f68317f3ebce8880cded96ccadf288770600
Petar Patev <petarpatev@abv.bg>
2024-06-08 13:03:31 +0300

    Implement login logic

--- src/views/login.js
+++ src/views/login.js
@@ -1,10 +1,11 @@
 import { html } from "../../node_modules/lit-html/lit-html.js";
+import { login } from "../api/user.js";
 
-const loginTemplate = () => html`
+const loginTemplate = (onSubmit) => html`
 <section id="login">
             <article>
                 <h2>Login</h2>
-                <form id="loginForm">
+                <form id="loginForm" @submit=${onSubmit}>
                     <label>E-mail: <input type="text" name="email"></label>
                     <label>Password: <input type="password" name="password"></label>
                     <input type="submit" value="Login">
@@ -14,5 +15,19 @@ const loginTemplate = () => html`
 `
 
 export const loginView = (ctx) => {
-    ctx.render(loginTemplate())
+
+    const onSubmit = async (e) => {
+
+        e.preventDefault();
+
+        const formData = new FormData(e.currentTarget);
+        const email = formData.get('email');
+        const password = formData.get('password');
+
+        await login(email, password);
+        e.target.reset();
+        ctx.page.redirect('/');
+    }
+
+    ctx.render(loginTemplate(onSubmit))
 }
