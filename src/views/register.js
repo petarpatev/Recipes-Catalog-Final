8c3ac5f59ff021c40da3cf500669a463d4fb50bb
Petar Patev <petarpatev@abv.bg>
2024-06-08 13:07:35 +0300

    Implement register logic

--- src/views/register.js
+++ src/views/register.js
@@ -1,10 +1,11 @@
 import { html } from "../../node_modules/lit-html/lit-html.js";
+import { register } from "../api/user.js";
 
-const registerTemplate = () => html`
+const registerTemplate = (onSubmit) => html`
 <section id="register">
             <article>
                 <h2>Register</h2>
-                <form id="registerForm">
+                <form id="registerForm" @submit=${onSubmit}>
                     <label>E-mail: <input type="text" name="email"></label>
                     <label>Password: <input type="password" name="password"></label>
                     <label>Repeat: <input type="password" name="rePass"></label>
@@ -15,5 +16,17 @@ const registerTemplate = () => html`
 `
 
 export const registerView = (ctx) => {
-    ctx.render(registerTemplate())
+    const onSubmit = async (e) => {
+
+        e.preventDefault();
+
+        const formData = new FormData(e.currentTarget);
+        const email = formData.get('email');
+        const password = formData.get('password');
+
+        await register(email, password);
+        e.target.reset();
+        ctx.page.redirect('/');
+    }
+    ctx.render(registerTemplate(onSubmit))
 }
